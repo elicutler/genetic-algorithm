64521316bd44fcdf68241c051ed608a0e36d6445
Eli Cutler <cutler.eli@gmail.com>
2019-05-30 22:26:03 -0700

    c

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='genetic_algorithm',
     version='0.1',
     description='Gentic algorithm for ML hyperparameter tuning of scikit-learn pipelines',
-    url='https://github.com/elicutler/genetic-algorithm',
+    url='https://github.com/elicutler/genetic_algorithm',
     author='Eli Cutler',
     author_email='cutler.eli@gmail.com',
     license='MIT',
