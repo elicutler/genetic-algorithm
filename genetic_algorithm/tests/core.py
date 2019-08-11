8df1b34d54a57932892128650a12c78095cd6cc1
Eli Cutler <cutler.eli@gmail.com>
2019-08-11 09:00:07 -0400

    finished tests for model_scorer (next is core)

--- genetic_algorithm/tests/core.py
+++ genetic_algorithm/tests/core.py
@@ -1,3 +1,22 @@
-from genetic_algorithm.core import (
-    GeneticAlgorithm, ModelMaker, ModelScorer, PipelineMaker
+from genetic_algorithm.core import GeneticAlgorithm
+from genetic_algorithm.utils.model_maker import ModelMaker
+from genetic_algorithm.utils.model_scorer import ModelScorer
+from genetic_algorithm.utils.pipeline_maker import PipelineMaker
+
+from genetic_algorithm.utils.default_logger import DefaultLogger
+
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import KFold
+
+n = 100
+X = pd.DataFrame({
+    'x1': np.random.normal(size=n), 
+    'g1': np.random.choice(['a', 'b', 'c'], size=n)
+})
+y = pd.Series(
+    0.5 + 0.5*X['x1'] + 1.0*(X['g1'] == 'b') + 1.5*(X['g1'] == 'c') 
+    + np.random.normal(size=n)
 )
--- genetic_algorithm/tests/utils/model_scorer.py
+++ genetic_algorithm/tests/utils/model_scorer.py
@@ -11,12 +11,18 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import KFold
 
 n = 100
-X = pd.DataFrame({'x1': np.random.normal(size=n)})
-y = pd.Series()
+X = pd.DataFrame({
+    'x1': np.random.normal(size=n), 
+    'g1': np.random.choice(['a', 'b', 'c'], size=n)
+})
+y = pd.Series(
+    0.5 + 0.5*X['x1'] + 1.0*(X['g1'] == 'b') + 1.5*(X['g1'] == 'c') 
+    + np.random.normal(size=n)
+)
 
 pipelineMaker = PipelineMaker(
     estimatorClass=GradientBoostingRegressor,
-    numFeatures=['x1', 'x2'], catFeatures=['g1', 'g2'],
+    numFeatures=['x1'], catFeatures=['g1'],
     randomState=617
 )
 pipeline = pipelineMaker.makePipeline(
@@ -28,12 +34,14 @@ pipeline = pipelineMaker.makePipeline(
     }
 )
 
-evalMetric = 'mean_squared_error'
+evalMetric = 'neg_mean_squared_error'
 crossValidator = KFold(n_splits=3, shuffle=True, random_state=617)
 
 modelScorer = ModelScorer(
-    X=X, y=y, evalMetric='neg_mean_squared_error', 
+    X=X, y=y, evalMetric=evalMetric, 
     crossValidator=crossValidator, errorScore=np.nan
 )
 
 modelScorer.scoreModel(pipeline=pipeline, aggregator='mean')
+
+logging.info('All tests passed')
