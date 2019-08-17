3cdddea3a3cb6b963f6ac527c6567e7f542e2c26
Eli Cutler <cutler.eli@gmail.com>
2019-08-17 16:43:05 -0700

    added PipelineMaker, ModelMaker, and ModelScorer to init

--- genetic_algorithm/__init__.py
+++ genetic_algorithm/__init__.py
@@ -1,3 +1,6 @@
 # Imports ==========
 
 from genetic_algorithm.core import GeneticAlgorithm
+from genetic_algorithm.utils.pipeline_maker import PipelineMaker
+from genetic_algorithm.utils.model_maker import ModelMaker
+from genetic_algorithm.utils.model_scorer import ModelScorer
